8200aa13b3d21a66278d20270fb0119ae7471cca
Stephen Gagnon <srgagnon@buffalo.edu>
2022-03-23 14:35:44 -0400

    exported additional functions experimental change of handling of glint direction

--- src/lightCurveModeling.jl
+++ src/lightCurveModeling.jl
@@ -6,6 +6,6 @@ include("types.jl")
 include("utilities.jl")
 include("lightCurveModel.jl")
 
-export targetObject, targetObjectFull, spaceScenario, Fobs, dFobs, _Fobs_Analysis, simpleSatellite, simpleScenario, customScenarioGenerator, customSatellite, customScenario
+export targetObject, targetObjectFull, spaceScenario, Fobs, dFobs, _Fobs, _Fobs_Analysis, simpleSatellite, simpleScenario, simpleScenarioGenerator, customScenarioGenerator, customSatellite, customScenario
 
 end # module
