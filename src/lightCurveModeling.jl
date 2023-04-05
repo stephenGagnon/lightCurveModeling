6f3fdb95193b47e117857903792f560f7b365a23
Stephen Gagnon <srgagnon@buffalo.edu>
2023-04-05 13:25:34 -0400

    updated exports

--- src/lightCurveModeling.jl
+++ src/lightCurveModeling.jl
@@ -1,13 +1,14 @@
 module lightCurveModeling
 using LinearAlgebra
 using attitudeFunctions
+using ForwardDiff
 # using Infiltrator
 
 include("types.jl")
 include("utilities.jl")
 include("lightCurveModel.jl")
 
-export targetObject, targetObjectFull, spaceScenario, Fobs, dFobs, _Fobs, _Fobs_preAlloc, _Fobs_Analysis, simpleSatellite, simpleScenario, simpleScenarioGenerator, customScenarioGenerator, objectGenerator2D, scenarioGenerator2D, customSatellite, customScenario, attLMFIM, _mapp
+export targetObject, targetObjectFull, spaceScenario, Fobs, dFobs, _Fobs, _Fobs_preAlloc, _Fobs_Analysis, simpleSatellite, simpleScenario, simpleScenario2D, simpleScenarioGenerator, customScenarioGenerator, objectGenerator2D, scenarioGenerator2D, customSatellite, customScenario, attLMFIM, _mapp, dFobs_2D, flatPlate3D, flatPlate3D_missing_section
 
 end # module
 #
